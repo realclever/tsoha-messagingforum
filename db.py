d7f1cb6cb3f3fd7c22e630f78dc3ce8fdeebbee6
Matt <matt@MBP.local>
2022-09-08 19:03:06 +0300

    db fix

--- db.py
+++ db.py
@@ -3,5 +3,5 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
